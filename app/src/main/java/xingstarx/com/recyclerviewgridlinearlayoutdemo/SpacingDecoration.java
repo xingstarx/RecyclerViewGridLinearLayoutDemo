389ca91428150dc5767c3a2da7646a3805fbc760
xingxing <xxx823952375@gmail.com>
2016-10-08 23:46:46 +0800

    SpacingDecoration 存在问题，第一个间隔太大

--- app/src/main/java/xingstarx/com/recyclerviewgridlinearlayoutdemo/SpacingDecoration.java
+++ app/src/main/java/xingstarx/com/recyclerviewgridlinearlayoutdemo/SpacingDecoration.java
@@ -3,7 +3,6 @@ package xingstarx.com.recyclerviewgridlinearlayoutdemo;
 import android.graphics.Rect;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 
 /**
@@ -34,9 +33,6 @@ public class SpacingDecoration extends RecyclerView.ItemDecoration {
         if (parent.getLayoutManager() instanceof GridLayoutManager) {
             GridLayoutManager lm = (GridLayoutManager) parent.getLayoutManager();
             GridLayoutManager.LayoutParams gridLp = (GridLayoutManager.LayoutParams) lp;
-
-            Log.e(TAG, "gridLp.getSpanSize() == " + gridLp.getSpanSize() + ", lm.getSpanCount() == " + lm.getSpanCount() +
-            "gridLp.getSpanIndex() == " + gridLp.getSpanIndex());
             if (gridLp.getSpanSize() == lm.getSpanCount()) {
                 if (isSetMarginLeftAndRight) {
                     leftOffset = mHorizontalSpacing;
@@ -53,27 +49,6 @@ public class SpacingDecoration extends RecyclerView.ItemDecoration {
                 }
             }
         }
-//        outRect.set(leftOffset, 0, rightOffset, bottomOffset);
-//        outRect.set(mHorizontalSpacing, mVerticalSpacing, 0, 0);
-
-
-        GridLayoutManager.LayoutParams layoutParams
-                = (GridLayoutManager.LayoutParams) view.getLayoutParams();
-
-        int position = layoutParams.getViewAdapterPosition();
-        if (position == RecyclerView.NO_POSITION) {
-            outRect.set(0, 0, 0, 0);
-            return;
-        }
-
-        // add edge margin only if item edge is not the grid edge
-        int itemSpanIndex = layoutParams.getSpanIndex();
-        // is left grid edge?
-        outRect.left = itemSpanIndex == 0 ? 0 : mHorizontalSpacing;
-        // is top grid edge?
-        outRect.top = itemSpanIndex == position ? 0 : mVerticalSpacing;
-        outRect.right = 0;
-        outRect.bottom = 0;
-
+        outRect.set(leftOffset, 0, rightOffset, bottomOffset);
     }
 }
